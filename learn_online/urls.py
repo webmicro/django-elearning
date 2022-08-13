9b9b9676422522df0a6b18c13d17c02823d9c0e5
Danish <danish@webmicrosystems.com>
2022-08-13 18:12:15 +0530

    Aug 13 code

--- learn_online/urls.py
+++ learn_online/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('team', views.team, name='team'),
     path('contact', views.contact, name='contact'),
     path('sign', views.sign, name='signup'),
+    path('login', views.my_login, name='login'),
+    path('logout', views.my_logout, name='logout'),    
     path('course-detail/<int:id>', views.course_detail, name='course-detail')
 ]
 
--- learn_online/views.py
+++ learn_online/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib import messages
 from .models import Experts, Contact, Course
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def home(request):
@@ -37,9 +39,55 @@ def contact(request):
         return render( request, 'contact.html')
 
 def sign(request):
+
+    if request.method == 'POST':
+        
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        conf_password = request.POST['conf_password']
+        is_error = False
+        if password != conf_password :
+           messages.error( request , 'Both password should be same') 
+           is_error = True
+
+        if User.objects.filter(email=email).exists() :
+            messages.error( request , 'You are already registered') 
+            is_error = True
+
+        if User.objects.filter(username=username).exists() :
+            messages.error( request , 'Username already exists') 
+            is_error = True    
+
+        if is_error == False:
+            obj_user = User.objects.create_user(username=username, password=password, email=email, first_name = fname, last_name= lname  )
+            obj_user.save()
+            return redirect('login')
+
     return render(request, 'signup.html')
 
+def my_login( request ):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        is_error = False
+        
+        user = authenticate( request, username=username, password=password)
+        if user is not None :
+            login( request, user )  
+            return redirect('/')            
+        else:
+            messages.error( request , 'Wrong username or password') 
+
+    return render(request, 'login.html')
 
 def course_detail( request, id ):
     cdata = get_object_or_404( Course, pk = id )    
     return render(request, 'course-detail.html', {'course': cdata})
+
+def my_logout( request ):
+    logout( request )
+    return redirect('/')               
